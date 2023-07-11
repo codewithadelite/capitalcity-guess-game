492360a262fd22937069b064b2717341407ccf16
Niyonshuti Shema Adelite <shemani2011@gmail.com>
2023-07-11 14:53:28 +0200

    Code formatted with Black

--- Game/models.py
+++ Game/models.py
@@ -3,76 +3,80 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
-class User(AbstractUser):
-	is_admin = models.BooleanField(default=False)
-	is_guest = models.BooleanField(default=False)
 
-	def __str__(self):
-		return self.username
+class User(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_guest = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
 
 class Admin(models.Model):
-	name = models.CharField(max_length=255)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now_add=True)
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
 
 class Guest(models.Model):
-	name = models.CharField(max_length=255)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now_add=True)
-	is_activated = models.BooleanField(default=False)
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_activated = models.BooleanField(default=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
 
 class Country(models.Model):
-	country_name = models.CharField(max_length=255)
-	city_name = models.CharField(max_length=255)
+    country_name = models.CharField(max_length=255)
+    city_name = models.CharField(max_length=255)
 
-	def __str__(self):
-		return self.country_name + " " + self.city_name
+    def __str__(self):
+        return self.country_name + " " + self.city_name
+
 
 class Quiz(models.Model):
-	name = models.CharField(max_length=255)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now_add=True)
-	is_activated = models.BooleanField(default=True)
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    is_activated = models.BooleanField(default=True)
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def is_done(self, request):
-		user = request.user
-		print(user)
-		quiz = self.id
-		done = QuizDone.objects.filter(quiz=quiz,user=user).count()
-		if done >= 1:
-			return True
-		else:
-			return False
+    def is_done(self, request):
+        user = request.user
+        print(user)
+        quiz = self.id
+        done = QuizDone.objects.filter(quiz=quiz, user=user).count()
+        if done >= 1:
+            return True
+        else:
+            return False
+
 
 class Answer(models.Model):
-	name = models.CharField(max_length=255)
-	is_country = models.BooleanField(default=False)
-	is_true = models.BooleanField(default=False)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now_add=True)
-	quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-	country = models.ForeignKey(Country, on_delete=models.CASCADE)
-	user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
+    name = models.CharField(max_length=255)
+    is_country = models.BooleanField(default=False)
+    is_true = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='')
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
+
 
 class QuizDone(models.Model):
-	quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now_add=True)
-
-
-	
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
--- Game/urls.py
+++ Game/urls.py
@@ -12,28 +12,35 @@ urlpatterns = [
 
     path('adminPanel/', views.adminIndex, name='adminIndex'),
     path('adminPanel/players', views.viewPlayers, name='viewPlayers'),
-    path('adminPanel/players/<str:pk>/status', views.statusPlayer, name='statusPlayer'),
+    path('adminPanel/players/<str:pk>/status',
+         views.statusPlayer, name='statusPlayer'),
 
     path('adminPanel/countries', views.viewCountries, name='viewCountries'),
     path('adminPanel/addCountry', views.addCountry, name='addCountry'),
-    path('adminPanel/<str:pk>/editCountry', views.editCountry, name='editCountry'),
-    path('adminPanel/<str:pk>/deleteCountry', views.deleteCountry, name='deleteCountry'),
+    path('adminPanel/<str:pk>/editCountry',
+         views.editCountry, name='editCountry'),
+    path('adminPanel/<str:pk>/deleteCountry',
+         views.deleteCountry, name='deleteCountry'),
 
     path('adminPanel/quiz', views.viewQuiz, name='viewQuiz'),
-    path('adminPanel/quiz/<str:pk>/status', views.statusQuiz, name='statusQuiz'),
+    path('adminPanel/quiz/<str:pk>/status',
+         views.statusQuiz, name='statusQuiz'),
     path('adminPanel/addQuiz', views.addQuiz, name='addQuiz'),
     path('adminPanel/<str:pk>/editQuiz', views.editQuiz, name='editQuiz'),
     path('adminPanel/<str:pk>/deleteQuiz', views.deleteQuiz, name='deleteQuiz'),
 
-    #PLAYER PANEL
+    # PLAYER PANEL
 
     path('playerPanel/', views.playerIndex, name='playerIndex'),
     path('playerPanel/listQuiz', views.listQuiz, name='listQuiz'),
-    path('playerPanel/<str:pk>/selectGuess', views.selectGuess, name='selectGuess'),
-    path('playerPanel/<str:pk>/playCountry', views.playCountry, name='playCountry'),
+    path('playerPanel/<str:pk>/selectGuess',
+         views.selectGuess, name='selectGuess'),
+    path('playerPanel/<str:pk>/playCountry',
+         views.playCountry, name='playCountry'),
     path('playerPanel/<str:pk>/playCity', views.playCity, name='playCity'),
     path('playerPanel/myMarks', views.myMarks, name='myMarks'),
-    path('playerPanel/<str:pk>/markDetail', views.markDetail, name='markDetail'),
+    path('playerPanel/<str:pk>/markDetail',
+         views.markDetail, name='markDetail'),
 
 
 ]
